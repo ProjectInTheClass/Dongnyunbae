2459406aefceb2121dd1cae80f6618baf95a9398
10kseok <76582376+10kseok@users.noreply.github.com>
2021-07-07 18:16:49 +0900

    tableViewProject
    7/7일자까지의 더미데이터로 테이블뷰를 구성해보았습니다.

---
+++ practiceProject/tableViewPractice/tableViewPractice/RestaurantCell.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantCell.swift
+//  tableViewPractice
+//
+//  Created by 추만석 on 2021/07/07.
+//
+
+import Foundation
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var meokbtiLabel: UILabel!
+    @IBOutlet weak var basisLabel: UILabel!
+    
+}
---
+++ practiceProject/tableViewPractice/tableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewPractice
+//
+//  Created by 추만석 on 2021/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
