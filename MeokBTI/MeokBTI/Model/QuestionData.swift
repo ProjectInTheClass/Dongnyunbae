2d249619de50013cf3f9400ad0e636e97e891595
csw8820 <swcheon90@gmail.com>
2021-09-23 21:08:37 +0900

    question change

--- MeokBTI/MeokBTI/Model/QuestionData.swift
+++ MeokBTI/MeokBTI/Model/QuestionData.swift
@@ -30,13 +30,13 @@ let questions: [Question] = [
 //                Answer(text: "강원도 까지 가서 막국수를 야무지게 즐긴다.", type: .accessPos), Answer(text: "집 근처 아무 막국수 집에 간다.", type: .accessNeg)]),
     
     Question(text: "Sns를 하다가 맛집을 발견했다. 하지만 서울에 있다고 한다.", answers: [
-                Answer(text: "대체할 다른 식당을 근처에서 찾아본다",
+                Answer(text: "가기 편한 비슷한 식당을 찾는다.",
                        type: .accessPos),
-                Answer(text: "라멘먹으러 일본까지 가는 나에겐 거리따위 신경쓰지 않는다. 무조건 간다.",
+                Answer(text: "내가 가고싶은 곳이면 거리는 중요하지 않다.",
                     type: .accessNeg)]),
     
     Question(text: "친구들과 탕수육을 먹기로 했다. 다들 아무데나 상관없다고 할 때 나는~", answers: [
-                Answer(text: "당장 배고프니까 여기서 3분 내에 있는 중국집에서 탕수육을 먹는다", type: .accessPos),
+                Answer(text: "가장 가까운 중국집에서 탕수육을 먹는다", type: .accessPos),
                 Answer(text: "이왕 먹는 거 15분 걷더라도 탕수육 맛집에서 먹는다.",
                     type: .accessNeg)]),
     
@@ -47,25 +47,25 @@ let questions: [Question] = [
                 Answer(text: "30cm로도 충분해.",
                     type: .ammountNeg)]),
     Question(text: "친구들과 치킨을 시킨다.", answers: [
-                Answer(text: "넷이서 두마리면 될 정도로 양이 많은 치킨을 시킨다.", type: .ammountPos), Answer(text: "치킨은 자고로 1인 1닭이다. 나눠먹기엔 적어도 혼자먹기는 적당한 치킨을 4마리 시킨다.", type: .ammountNeg)]),
+                Answer(text: "넷이서 두마리면 될 정도로 양이 많은 치킨을 시킨다.", type: .ammountPos), Answer(text: "1인 1닭 할 수 있는 양의 치킨집에서 4마리를 시킨다.", type: .ammountNeg)]),
     
     Question(text: "이틀 뒤 중요한 시험을 치르게 된다. 혼자 도서관을 가서 공부하다 보니 점심시간이 되었다.", answers: [
-                Answer(text: "시험이 얼마 남지 않았으니 최대한 간단하게 먹고 다시 공부하러 간다.", type: .ammountPos), Answer(text: "든든하게 먹어야 다시 힘내서 공부를 할 수 있다.", type: .ammountNeg)]),
+                Answer(text: "많이 먹으면 졸리니까 적당히 먹는다.", type: .ammountPos), Answer(text: "든든하게 먹어야 다시 힘내서 공부를 할 수 있다.", type: .ammountNeg)]),
     
-    Question(text: "친구들이 우리집에 놀러왔다. 비빔면을 끓여먹으려는데 3개를 끓이자니 애매하고 4개를 끓이자니 많다.", answers: [
-                Answer(text: "음식을 남길 순 없으니 적당히 먹을 수 있게 3개만 끓인다.", type: .ammountPos), Answer(text: "부족한 것 보단 차라리 남기는 게 나으니 4개를 끓인다.", type: .ammountNeg)]),
+    Question(text: "친구들이 우리집에 놀러왔다. 비빔면을 해먹으려는데 3개는 애매하고 4개는 많다.", answers: [
+                Answer(text: "부족한 것 보단 차라리 남기는 게 나으니 4개를 끓인다.", type: .ammountPos), Answer(text: "음식을 남길 순 없으니 적당히 먹을 수 있게 3개만 끓인다.", type: .ammountNeg)]),
     
     Question(text: "나는 현재 군인이다. 삽겹살이 너무 먹고 싶다. 마침 휴가를 나와서 삽겹살 무한 리필집에 오게 되었다. 너무 많이 먹어서 현재 배가 부른 상태다.", answers: [
-                Answer(text: "언제 다시 먹을지 모르니 배 불러도 더 먹는다", type: .ammountPos), Answer(text: "배부르니 만족. 시마이 친다.", type: .ammountNeg)]),
+                Answer(text: "언제 다시 먹을지 모르니 배 불러도 더 먹는다", type: .ammountPos), Answer(text: "모자라게 먹는게 더 맛있다. 적당히 먹고 만다.", type: .ammountNeg)]),
     
-//    Question(text: "오늘은 치팅데이! 마음껏 먹어보자.", answers: [
-//                Answer(text: "세상엔 맛있는 게 너무 많다. 이것저것 다 시켜 먹는다.", type: .ammountPos), Answer(text: "어..어? 내 생각보다 많이 들어가지 않아 아쉬워한다.", type: .ammountNeg)]),
+    Question(text: "오늘은 치팅데이! 마음껏 먹어보자.", answers: [
+                Answer(text: "세상엔 맛있는 게 너무 많다. 이것저것 다 시켜 먹는다.", type: .ammountPos), Answer(text: "치팅데이라도 적당히 먹는다.", type: .ammountNeg)]),
     
     
     
 
     // Mood
-    Question(text: "연인과에 1주년, 점심을 먹으려고 한다.", answers: [
+    Question(text: "연인과 1주년, 점심을 먹으려고 한다.", answers: [
                 Answer(text: "맛은 모르겠지만 인테리어가 예쁘다고 소문난  식당에 간다",
                     type: .moodPos),
                 Answer(text: "식당은 음식을 먹기 위해 가는 곳이다. 음식이 맛있다면 분위기는 상관 없다.",
@@ -76,31 +76,32 @@ let questions: [Question] = [
     
     Question(text: "어디서 더 먹고 싶은가?", answers: [
                 Answer(text: "전문 라면집에서 먹는 라면.", type: .moodPos),
-                Answer(text: "야간 행군 끝나고 먹는 라면.", type: .moodNeg)]),
+                Answer(text: "김밥천국에서 먹는 라면.", type: .moodNeg)]),
     
-    Question(text: "연구실 프로젝트 도중 밥 시간이 되었다. 모처럼 교수님께서 한턱 쏘신다고 하는데 한식을 드시고 싶다고 하신다. 이럴 때,", answers: [
-                Answer(text: "보기 좋아야 먹기도 좋다고 비싸더라도 고풍스러운 한식당을 소개시켜드린다.", type: .moodPos),
-                Answer(text: "멋없더라도 할머님들의 손맛이 알차고 자연스럽게 담긴 한식당을 소개시켜드린다.", type: .moodNeg)]),
+    Question(text: "모처럼 교수님께서 한턱 쏘신다고 하는데 한식을 드시고 싶다고 하신다. 이럴 때", answers: [
+                Answer(text: "누가봐도 고풍스러운 한옥집을 추천한다.", type: .moodPos),
+                Answer(text: "김치찌개도 한식아니야? 학교앞 김치찌개 가게를 추천한다.", type: .moodNeg)]),
     
     // price
+    // 비싼게 pos 싼게 neg
     Question(text: "아직 월급날은 3일 남았고 내 잔고는 바닥을 치고 있다.", answers: [
-                Answer(text: "식비부터 최대한 아껴야하므로 남은 3일간은 대충 끼니를 해결한다.", type: .pricePos),
-                Answer(text: "잘 먹는게 최고니 다른 곳에서 아끼고 먹는 것 만큼은 아끼지 않는다.",
+                Answer(text: "나는 먹는게 무엇보다 중요하니까 다른 생활비를 아껴서 식비로 쓴다.", type: .pricePos),
+                Answer(text: "아끼는건 식비가 가장 쉽다. 식비를 아끼자.",
                     type: .priceNeg)]),
     Question(text: "드디어 월급날, 돈이 들어온 기념으로 무언가 특별한 음식을 찾다보니 초밥이 끌린다.", answers: [
-                Answer(text: "이왕 먹는거 하나하나 정성스럽게 만들어주는 곳을 가보자. 오마카세 식당에 간다.", type: .pricePos),
-                Answer(text: "초밥이 거기서 거기지. 가격이 그렇게 비싸지 않은 동네 초밥집에 간다.", type: .priceNeg)]),
+                Answer(text: "고오급 초밥집 오마카세 식당으로 간다.", type: .pricePos),
+                Answer(text: "아무렴 좋다. 초밥 파는 아무 곳이나 간다.", type: .priceNeg)]),
     
     Question(text: "친구 생일날 밥 한끼를 사주려고 한다.", answers: [
-                Answer(text: "친구가 자주가는 식당에서 한 턱 쏜다.", type: .pricePos),
-                Answer(text: "가격이 좀 들더라도 친구가 못 먹어본 음식을 대접한다.", type: .priceNeg)]),
+                Answer(text: "가격이 좀 들더라도 친구가 못 먹어본 음식을 대접한다.", type: .pricePos),
+                Answer(text: "친구가 자주가는 식당에서 한 턱 쏜다.", type: .priceNeg)]),
     
     Question(text: "브리또(또는 그 무언가) 를 먹을거다.", answers: [
-                Answer(text: "먹고 싶은 토핑 하나만 정해서 넣는다.", type: .pricePos),
-                Answer(text: "비싸더라도 먹고 싶은 토핑 이것저것 다 넣는다.", type: .priceNeg)]),
+                Answer(text: "비싸더라도 먹고 싶은 토핑 이것저것 다 넣는다.", type: .pricePos),
+                Answer(text: "먹고 싶은 토핑 하나만 정해서 넣는다.", type: .priceNeg)]),
     
     Question(text: "짬뽕을 먹고 싶을 때", answers: [
-                Answer(text: "동네 중국집에서 편하고 저렴하게 먹어야지.", type: .pricePos),
-                Answer(text: "비싸더라도 이비가짬뽕 같은 이름있는 곳에서 먹어야지.", type: .priceNeg)])
+                Answer(text: "비싸더라도 이비가짬뽕 같은 이름있는 곳에서 먹어야지.", type: .pricePos),
+                Answer(text: "동네 중국집에서 편하고 저렴하게 먹어야지.", type: .priceNeg)])
 // END
 ]
